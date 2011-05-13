de90d4da1e8f47b8e6e39546211e6586943ef00f
Sam Thompson <sam@websyntax.net>
2011-05-12 19:48:11 -0700

    Added other elements of the DBAL

---
+++ Format/Types/FormatInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * @package     OpenFlame Dbal
+ * @copyright   (c) 2011 OpenFlameCMS.com
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Dbal
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Dbal\Formats\Types;
+
+if(!defined('OpenFlame\\ROOT_PATH')) exit;
+
+/**
+ * OpenFlame Dbal - Formats
+ * 	     Provides infrastructure for data to be read and written to different formats
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Dbal
+ */
+interface FormatInterface
+{
+}
---
+++ Versioner/Driver.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * @package     OpenFlame Dbal
+ * @copyright   (c) 2011 OpenFlameCMS.com
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Dbal
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Dbal\Versioner;
+
+if(!defined('OpenFlame\\ROOT_PATH')) exit;
+
+/**
+ * OpenFlame Dbal - Versioner
+ * 	     Versions your database schemas in a DBMS neutral format
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Dbal
+ */
+class Driver
+{
+}
