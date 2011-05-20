9bb3a4df6930f7b07cee89a14f83f4265ad5bfb5
Sam Thompson <sam@websyntax.net>
2011-05-20 14:51:36 -0700

    Working on Query a little more

--- Query.php
+++ Query.php
@@ -23,25 +23,93 @@ if(!defined('OpenFlame\\ROOT_PATH')) exit;
  */
 class Query
 {
-	private $pdo; 
+	/*
+	 * @var PDO connection
+	 */
+	private $pdo;
 
-	public static function getInstance($id = 0)
+	/*
+	 * @var PDO Statement
+	 */
+	private $stmt;
+
+	/*
+	 * @var SQL to be ran
+	 */
+	protected $sql = '';
+
+	/*
+	 * @var Statement params to be bound
+	 */
+	protected $params = array();
+
+	/*
+	 * Statically get an instance
+	 * @param string $name - Connection name
+	 * @return new \OpenFlame\Dbal\Query
+	 */
+	public static function getInstance($name = '')
 	{
-		return new static($id);
+		return new static($name);
 	}
 
-	public function __construct($id = 0)
+	/*
+	 * Normal constructor
+	 * @param string $name - Connection name
+	 */
+	public function __construct($name = '')
 	{
-		$this->pdo = \OpenFlame\Dbal\Connection::get($id);
+		$name = empty($name) ? \OpenFlame\Dbal\Connection::DEFAULT_CON_NAME : (string) $name;
+
+		$this->pdo = \OpenFlame\Dbal\Connection::getInstance($name)->get();
 	}
 
+	/*
+	 * Set the SQL to be ran
+	 * @param string $sql 
+	 * @return \OpenFlame\Dbal\Query - provides fluent interface 
+	 */
 	public function sql($sql)
 	{
+		$this->sql = (string) $sql;
+
 		return $this;
 	}
 
+	/*
+	 * Set params
+	 * @param array $params - to run through PDO's prepared statements
+	 * @return \OpenFlame\Dbal\Query - provides fluent interface 
+	 */
+	public function setParams($params = array())
+	{
+		$this->params = (array) $params;
+
+		return $this;
+	}
+
+	/**
+	 * Query and fetch a row 
+	 * @note - Safe for itteration
+	 * @return array - Associative array of the row being fetched
+	 */
+	public function fetchRow()
+	{
+	}
+
+	/**
+	 * Query and fetch the rowset
+	 * @return array - Multi-dimensional associative array of the rowset being fetched
+	 */
+	public function fetchRowset()
+	{
+	}
+
+	/**
+	 * Excecute a query
+	 * @return int - Number of rows affected
+	 */
 	public function exec()
 	{
-		return $this;
 	}
 }
