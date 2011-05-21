58d1000f95c0d2e32650a0d441c0ee32dd17153a
Sam Thompson <sam@websyntax.net>
2011-05-20 20:24:35 -0700

    Added the first clause collectors

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -23,4 +23,141 @@ if(!defined('OpenFlame\\ROOT_PATH')) exit;
  */
 class QueryBuilder extends Query
 {
+	/*
+	 * @var flag - Type of query (determined by the first clause)
+	 */
+	protected $type = -1;
+
+	/*
+	 * @var array - Fields to select
+	 */
+	protected $select = array();
+
+	/*
+	 * @var array - Tables that being affect in this query
+	 */
+	protected $tables = array();
+
+	/*
+	 * consts - Query types
+	 */
+	const TYPE_SELECT = 0;
+	const TYPE_UPDATE = 1;
+	const TYPE_INSERT = 2;
+	const TYPE_DELETE = 3;
+	const TYPE_UPSERT = 4;
+
+	/*
+	 * Statically get an instance
+	 * @param string $name - Connection name
+	 * @return new \OpenFlame\Dbal\Query
+	 */
+	public static function getInstance($name = '')
+	{
+		return new static($name);
+	}
+
+	/*
+	 * Normal constructor
+	 * @param string $name - Connection name
+	 */
+	public function __construct($name = '')
+	{
+		$this->pdo = \OpenFlame\Dbal\Connection::getInstance($name)->get();
+	}
+
+	/**
+	 * Start a SELECT statement
+	 * @param mixed - Fields to select
+	 * @return \OpenFlame\Dbal\QueryBuilder - Provides a fluent interface.
+	 */
+	public function select($fields)
+	{
+		$this->select = array_merge($this->select, $this->normalizeArray($fields));
+		$this->type = static::TYPE_SELECT;
+
+		return $this;
+	}
+
+	/**
+	 * Start an UPDATE statement
+	 * @param mixed - tables
+	 * @return \OpenFlame\Dbal\QueryBuilder - Provides a fluent interface.
+	 */
+	public function update($tables)
+	{
+		$this->tables = array_merge($this->tables, $this->normalizeArray($tables));
+		$this->type = static::TYPE_UPDATE;
+
+		return $this;
+	}
+
+	/**
+	 * Start an INSERT statement
+	 * @param mixed - tables
+	 * @return \OpenFlame\Dbal\QueryBuilder - Provides a fluent interface.
+	 */
+	public function insert($table)
+	{
+		$this->tables = array_slice($this->normalizeArray($table), 0, 1);
+		$this->type = static::TYPE_INSERT;
+
+		return $this;
+	}
+
+	/**
+	 * Start an UPSERT statement
+	 * @param mixed - tables
+	 * @return \OpenFlame\Dbal\QueryBuilder - Provides a fluent interface.
+	 */
+	public function upsert($table)
+	{
+		$this->tables = array_slice($this->normalizeArray($table), 0, 1);
+		$this->type = static::TYPE_UPSERT;
+
+		return $this;
+	}
+
+	/**
+	 * Start a DELETE statement
+	 * @param mixed - tables
+	 * @return \OpenFlame\Dbal\QueryBuilder - Provides a fluent interface.
+	 */
+	public function delete($tables)
+	{
+		$this->tables = $this->normalizeArray($tables);
+		$this->type = static::TYPE_DELETE;
+
+		return $this;
+	}
+
+	/**
+	 * FROM clause
+	 * @param mixed - Single table
+	 * @return \OpenFlame\Dbal\QueryBuilder - Provides a fluent interface.
+	 */
+	public function from($tables)
+	{
+		$this->tables = array_merge($this->tables, $this->normalizeArray($tables));
+
+		return $this;
+	}
+
+	/**
+	 * Used to (internally) normalize statements to an array
+	 * @todo change foreach() to an array_map() implementation
+	 * @param mixed - array or commma separated data
+	 * @return array - Normalized data
+	 */
+	protected function normalizeArray($items)
+	{
+		if(!is_array($items))
+		{
+			$items = explode(',', $items);
+		}
+
+		$items = array_map('trim', $items);
+
+		return $items;
+	}
 }
