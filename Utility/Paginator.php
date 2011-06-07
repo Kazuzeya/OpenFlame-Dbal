ae9c47ce63148ec8c9d9bad0e567614f65a73e1c
Sam Thompson <sam@websyntax.net>
2011-06-07 12:05:03 -0700

    Started work on the Paginator and Searcher

---
+++ Utility/Paginator.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * @package     OpenFlame Dbal
+ * @copyright   (c) 2011 openflame-project.org
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Dbal
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Dbal\Utility;
+
+if (!defined('OpenFlame\\ROOT_PATH')) exit;
+
+/**
+ * OpenFlame Dbal - Paginator
+ * 	     Provides a nice interface for paginating queries
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Dbal
+ */
+class Paginator
+{
+	/*
+	 * @var per page limit
+	 */
+	protected $limit = 50;
+
+	/*
+	 * @var selection page number
+	 */
+	protected $pageNumber = 0;
+
+	/*
+	 * Get instance
+	 */
+	public static function getInstance()
+	{
+		return new static();
+	}
+
+	/*
+	 * Set the base query
+	 * @param mixed $query - Instance of Query, QueryBuilder, or a string
+	 * @return \OpenFlame\Dbal\Utility\Paginator - provides fluent interface 
+	 */
+	public function setQuery($query)
+	{
+	}
+
+	/*
+	 * Set the page limit
+	 * @param int $limit - Limit for selection (number of items/page)
+	 * @return \OpenFlame\Dbal\Utility\Paginator - provides fluent interface 
+	 */
+	public function setPageLimit($limit)
+	{
+		$this->limit = (int) $limit;
+
+		return $this;
+	}
+
+	/*
+	 * Set the current page number
+	 * @param int $limit - Limit for selection (number of items/page)
+	 * @return \OpenFlame\Dbal\Utility\Paginator - provides fluent interface 
+	 */
+	public function setPageNumber($page)
+	{
+		$this->pageNumber = (int) $page;
+
+		return $this;
+	}
+
+	/*
+	 * Fetch the row (Itteration safe)
+	 * @return array - Associative array representing the current row
+	 */
+	public function fetchRow()
+	{
+	}
+
+	/*
+	 * Fetch the rowset
+	 * @return array - Multi-dimensional array representing the row set returned
+	 */
+	public function fetchRowset()
+	{
+	}
+
+	public function getCurrentPage()
+	{
+		return $this->pageNumber;
+	}
+
+	public function getTotalPages()
+	{
+	}
+
+	public function getTotalRecords()
+	{
+	}
+
+	private function _query()
+	{
+	}
+}
