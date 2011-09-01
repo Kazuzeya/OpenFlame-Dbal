20c5763d57e0ea0810508456f21c905a3926fcfd
Sam Thompson <sam@websyntax.net>
2011-08-31 22:27:11 -0700

    Started with QueryBulder

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -19,6 +19,46 @@ namespace OpenFlame\Dbal;
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        https://github.com/OpenFlame/OpenFlame-Dbal
  */
-class QueryBuilder extends Query
+class QueryBuilder
 {
+	/*
+	 * Instance of \OpenFlame\Dbal\Query() (or an extending class)
+	 * @var object \OpenFlame\Dbal\Query()
+	 */
+	private $query;
+
+	/*
+	 * Get a new instnace the fluent interface way
+	 * @param object (optional) - Instance of a custom Driver inheriting \OpenFlame\Dbal\Query()
+	 * -- OR --
+	 * @param string (optional) - Connection name, defaults to default conneciton
+	 *
+	 * @return instance of QueryBuilder
+	 */
+	public static function newInstance($arg = null)
+	{
+		return new static($arg);
+	}
+
+	/*
+	 * Constructor
+	 * @param object (optional) - Instance of a custom Driver inheriting \OpenFlame\Dbal\Query()
+	 * -- OR --
+	 * @param string (optional) - Connection name, defaults to default conneciton
+	 */
+	public function __construct($arg = null)
+	{
+		// Check to see if it is an instance of Query (or a class extending that)
+		if ($arg instanceof \OpenFlame\Dbal\Query)
+		{
+			$this->query = $arg;
+		}
+		// Next is default query handing with a non-default connection
+		else
+		{
+			$name = (string) $arg;
+			$class = '\\OpenFlame\\Dbal\\DBMS\\' . Connection::getInstance($name)->getDriver();
+			$this->query = new $class($name);
+		}
+	}
 }
